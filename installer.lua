5b4baccb4305f414eceda590a4871f1aff49b4b6
Interfiber <webmaster@interfiber.dev>
2022-03-27 17:06:26 -0400

    add the most basic installer of all time

---
+++ installer.lua
@@ -0,0 +1,4 @@
+print("installing coredoc to /usr/local/coredoc")
+os.execute("sudo git clone https://github.com/Interfiber/coredoc.git /usr/local/coredoc")
+print("cloned, add the following line to you're shell profile:")
+print("export PATH=/usr/local/coredoc:$PATH")
